fd19083083887f892a01dba94056babf60c07d44
darbic <tomas.klima@aiteq.com>
2017-08-31 21:39:09 +0200

    CLI groups registration refactored

--- src/cli/mb-util.ts
+++ src/cli/mb-util.ts
@@ -4,7 +4,6 @@ import { logger } from "../logger";
 import { BotUtils } from "../utils/bot-utils";
 import { cliout } from "./cli-logger";
 import { Group } from "./group";
-import * as groups from "./groups/";
 
 /* tslint:disable no-console */
 
@@ -12,11 +11,6 @@ import * as groups from "./groups/";
  * The entrypoint of the mbutil CLI.
  */
 export class MBUtil {
-
-    public static registerGroup(group: Group): void {
-
-        MBUtil.groups.set(group.getName(), group);
-    }
 
     public static getGlobalOptions(): string {
         return `Global options:
@@ -27,22 +21,31 @@ export class MBUtil {
 `;
     }
 
-    private static groups: Map<string, Group> = new Map<string, Group>();
+    private groups: Map<string, Group> = new Map<string, Group>();
 
-    private static exitWithUsage(): void {
+    private exitWithUsage(): void {
         console.log("Usage: mbutil <group> [command] [options]");
-        console.log("Groups: ", Array.from(MBUtil.groups.keys()).join(", "));
+        console.log("Groups: ", Array.from(this.groups.keys()).join(", "));
         console.log("\nType 'mbutil <group> --help' to display usage for the group\n");
         process.exit(0);
     }
 
     private botUtils: BotUtils;
 
-    constructor() {
-        Object.keys(groups).forEach((key) => new groups[key]());
+    private registerGroups() {
+
+        // read all groups from groups/index.js
+        let groups = require("./groups/");
+
+        Object.keys(groups).forEach((key) => {
+            let group: Group = new groups[key]();
+            this.groups.set(group.getName(), group);
+        });
     }
 
     public async bootstrap(): Promise<void> {
+
+        this.registerGroups();
 
         const options: any = minimist(process.argv.slice(2));
 
@@ -50,9 +53,9 @@ export class MBUtil {
 
         const [group, command] = options._;
 
-        const groupHandler: Group = MBUtil.groups.get(group);
+        const groupHandler: Group = this.groups.get(group);
 
-        groupHandler || MBUtil.exitWithUsage();
+        groupHandler || this.exitWithUsage();
 
         options.help && groupHandler.exitWithUsage();
 
